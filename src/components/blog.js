72bbf07ea0f8774dd0e8f328287081a59b378a13
TravisFriday <Travisgifriday@gmail.com>
2019-08-22 12:38:06 -0700

    added blog page

---
+++ src/components/blog.js
@@ -0,0 +1,20 @@
+import React, { Component } from "react";
+
+class Blog extends Component {
+  render() {
+    return (
+      <div className="about">
+        <h1>My Blog</h1>
+        <p>
+          Hello everybody, welcome to my blog. This series will tell stories all
+          about me. Whether it's about my career as a software engineer, my
+          hobbies, and things that has happened to me, good and bad. I will be
+          genuine, not highlighting my successes but also my failures, and how I
+          overcome those failures.
+        </p>
+      </div>
+    );
+  }
+}
+
+export default Blog;
--- src/components/main.js
+++ src/components/main.js
@@ -1,12 +1,12 @@
-import React from 'react';
-import { Switch, Route } from 'react-router-dom';
+import React from "react";
+import { Switch, Route } from "react-router-dom";
 
-import LandingPage from './landingpage';
-import AboutMe from './aboutme';
-import Contact from './contact';
-import Projects from './projects';
-import Resume from './resume';
-
+import LandingPage from "./landingpage";
+import AboutMe from "./aboutme";
+import Contact from "./contact";
+import Projects from "./projects";
+import Resume from "./resume";
+import Blog from "./blog";
 
 const Main = () => (
   <Switch>
@@ -15,7 +15,8 @@ const Main = () => (
     <Route path="/contact" component={Contact} />
     <Route path="/projects" component={Projects} />
     <Route path="/resume" component={Resume} />
+    <Route path="/blog" component={Blog} />
   </Switch>
-)
+);
 
 export default Main;
